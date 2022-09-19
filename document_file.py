f961f2f70289c4bd830d378536987ecc56050823
Swapnil Adhav <89259682+SwapnilAdhav10@users.noreply.github.com>
2022-09-19 09:46:45 +0530

    Add files via upload

---
+++ document_file.py
@@ -0,0 +1,14 @@
+import os
+
+log_path = os.getcwd()
+client_id= '8S56FEB6JI-100'
+secret_key='BGOWTLDD3I'
+redirect_url = 'https://www.google.com'
+username= 'XV18626'
+password='Vishv@15'
+pin1=9
+pin2=1
+pin3=4
+pin4=4
+response_type='code'
+grant_type='authorization_code'
